9d6d926c6da755f243c6409604409ee915edd805
Adam Jackson <akjackson1@gmail.com>
2021-01-31 19:59:27 -0500

    Calculate lat/lng bounds

--- api/controller.js
+++ api/controller.js
@@ -1,12 +1,26 @@
 import puppeteer from 'puppeteer';
+import axios from 'axios';
+import { response } from 'express';
 
 export const index = async (req, res) => {
+	//To do: set constants from params
+	const speciesCode = 'coohaw';
+	const lat = 41.155599;
+	const lng = -81.508896;
+	const distance = 10;
+
 	const locations = await getLocationsWithinBounds();
 	if (!locations) {
 		res.send('Error retrieving species locations');
 	}
-	res.send('Success!');
-	
+	const speciesInfo = await getSpeciesInfo(speciesCode);
+	if (!speciesInfo || speciesInfo?.category !== 'species') {
+		res.send('Error retrieving species');
+	}
+	const speciesName = speciesInfo.comName;
+	const bounds = getBounds(lat, lng, distance);
+
+	res.send('Success');
 }
 
 const getLocationsWithinBounds = async () => {
@@ -17,6 +31,15 @@ const getLocationsWithinBounds = async () => {
 		json = JSON.parse(response);
 	} catch (error) {}
 	return json;
+}
+
+const getSpeciesInfo = async (speciesCode) => {
+	try {
+		const response = await axios.get(`https://api.ebird.org/v2/ref/taxonomy/ebird?species=${speciesCode}&fmt=json`);
+		return response.data[0];
+	} catch (error) {
+		return false;
+	}
 }
 
 const getSecureEbirdContent = async (url) => {
@@ -38,4 +61,28 @@ const getSecureEbirdContent = async (url) => {
 	const text = await response.text();
 	await browser.close();
 	return text;
-};
+}
+
+const getBounds = (lat, lng, distance, unit = 'km') => {
+	const earth_radius = unit === 'km' ? 6371.009 : 3958.761;
+	
+	// latitude boundaries
+	const maxLat = lat + radiansToDegrees(distance / earth_radius);
+	const minLat = lat - radiansToDegrees(distance / earth_radius);
+
+	// longitude boundaries
+	const maxLng = lng + radiansToDegrees(distance / earth_radius) / Math.cos(degreesToRadians(lat));
+	const minLng = lng - radiansToDegrees(distance / earth_radius) / Math.cos(degreesToRadians(lat));
+
+	return { maxLat, minLat, maxLng, minLng }
+}
+
+const radiansToDegrees = (radians) => {
+	const pi = Math.PI;
+	return radians * (180/pi);
+}
+
+const degreesToRadians = (radians) => {
+	const pi = Math.PI;
+	return radians * (pi/180);
+}
